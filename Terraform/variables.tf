e007bde9d571d7d42d33336d22e2a9e43e71e38d
Ajinkya <ajinkyaa3xd@gmail.com>
2026-09-12 10:31:48 +0000

    added all the variables

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -2,3 +2,36 @@ variable "region" {
   type    = string
   default = "ap-south-1"
 }
+
+variable "instance_type" {
+  type = string
+  default = "t3.medium"
+}
+
+# variables.tf additions
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "kubeadm-lab"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {
+    ManagedBy   = "terraform"
+  }
+}
+
+variable "worker_count" {
+  type = number
+  default = 2 
+}
+
+variable "key_name" {
+  type = string
+  default = "ec2"
+}
