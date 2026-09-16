f71d22b6671ae1181dfbfa98f6a8880cf3d61f04
Ajinkya <ajinkyaa3xd@gmail.com>
2026-09-16 06:16:25 +0000

    added control plane private ip in output

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -2,6 +2,10 @@ output "control_plane_public_ip" {
   value = aws_instance.control_plane.public_ip
 }
 
+output "control_plane_privat_ip" {
+  value = aws_instance.control_plane.private_ip
+}
+
 output "worker_public_ips" {
   value = aws_instance.worker[*].public_ip
 }
